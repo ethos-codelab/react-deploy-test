7bd3bc0d4526b0aca55df91fb2786b3f741757fe
Sanchit <sanchit.gupta@ethoswatches.com>
2020-11-03 17:49:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import './App.css';
+import Image1 from './c.jpg';
+import Image2 from './b.jpg';
+import Image3 from './a.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {
+            <AwesomeSlider animation="cubeAnimation">
+              <div data-src={Image1} />
+              <div data-src={Image2} />
+              <div data-src={Image3} />
+            </AwesomeSlider>
+        }
       </header>
     </div>
   );
 }
 
 export default App;
+// <select>
+// <option value="cubeAnimation">Cube Animation</option>
+// <option value="fallAnimation">Fall Animation</option>
+// <option value="foldOutAnimation">Fold Out Animation</option>
+// <option value="openAnimation">Open Animation</option>
+// <option value="scaleOutAnimation">Scale Out Animation</option>
+// </select>
